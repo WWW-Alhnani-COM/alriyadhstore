a2e283fa13ed5f96b017e16301de9e7b78de7b7a
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-05-16 06:57:09 +0300

    Update main.tsx

--- Asset-Manager/artifacts/store/src/main.tsx
+++ Asset-Manager/artifacts/store/src/main.tsx
@@ -2,8 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { setBaseUrl } from "api-client-react";
-
-setBaseUrl(import.meta.env.VITE_API_URL);
-
 createRoot(document.getElementById("root")!).render(<App />);
