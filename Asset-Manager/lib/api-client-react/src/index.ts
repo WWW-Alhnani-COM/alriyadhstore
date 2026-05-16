3f56fbfef208778c6cb566af371281188a88d2d3
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-05-16 06:50:58 +0300

    Update index.ts

--- Asset-Manager/lib/api-client-react/src/index.ts
+++ Asset-Manager/lib/api-client-react/src/index.ts
@@ -1,3 +1,4 @@
+export { setBaseUrl } from "./custom-fetch";
 export * from "./generated/api";
 export * from "./generated/api.schemas";
 export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
