8d6087605f7b9316e1035bf52854f6c04bae8868
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-05-17 05:28:19 +0300

    Delete Asset-Manager/artifacts/api-server/pgSession.js

--- Asset-Manager/artifacts/api-server/pgSession.js
+++
@@ -1,9 +0,0 @@
-const pgSession = require('connect-pg-simple')(session);
-const { Pool } = require('pg');
-
-const pgPool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = { pgSession, pgPool };
