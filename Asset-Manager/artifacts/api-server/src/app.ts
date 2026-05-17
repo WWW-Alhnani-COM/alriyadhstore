90371ab6cb2e924aeb9daf4f95a03666b5ac4af4
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-05-17 04:39:49 +0300

    Update app.ts

--- Asset-Manager/artifacts/api-server/src/app.ts
+++ Asset-Manager/artifacts/api-server/src/app.ts
@@ -26,9 +26,8 @@ app.use(
     },
   }),
 );
-app.use(cors({ 
-  origin: 'https://riyadhstore.vercel.app',
-  credentials: true 
+app.use(cors({
+  origin: '*'
 }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
